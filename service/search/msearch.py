112b1df3f3f45de7c33174e7b1940c7bdae32ddc
GTouch <36075936+gtouchgogo@users.noreply.github.com>
2019-05-15 13:54:26 +0800

    Update msearch.py

--- service/search/msearch.py
+++ service/search/msearch.py
@@ -1,154 +1,141 @@
-# -*- encoding:utf-8 -*-
-
-from flask import Blueprint, request, jsonify
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'jingyu.he'
 import json
+from flask import request, jsonify, render_template, Blueprint
+import base64
+import requests
+import asyncio
+import time
+from utils.utility import Utility
+from conf.constants import *
+from utils.authorization import check_ckey
 from utils.request_util import RequestUtil
-from utils.common_sql import UserLib
-from utils.get_conf import get_config_file, get_logger_file
+from utils.get_conf import *
+from conf.sharemsg_params_define import *
 from utils.logger_conf import configure_logger
 
-search_blueprint = Blueprint('search', __name__)
-
-
-# -------------------------- 生成logger --------------------------
 log_path = get_logger_file()
-log_path = log_path + '_search.log'
-logger = configure_logger('main', log_path)
+log_path = log_path + 'sharemsg.log'
+sharemsg_logger = configure_logger('sharemsg', log_path)
 
-# -------------------------- 读取默认配置 --------------------------
-config = get_config_file()
-is_check_ckey = config['qtalk'].getboolean('ckey_check')
-if is_check_ckey:
-    logger.info("CKEY AUTHORIZATION INITIALING...")
-    from utils.authorization import check_ckey
-single_portrait = config['qtalk']['single_portrait']
-muc_portrait = config['qtalk']['muc_portrait']
-
-# -------------------------- 既定的配置 --------------------------
-userGroup = 'Q01'
-groupGroup = 'Q02'
-singlekeywordGroup = 'Q05'
-muckeywordGroup = 'Q06'
-commonGroup = 'Q07'
-GroupDetail = 'Q10'
-
-QTALK_OPEN_USER_VCARD = 0  # 打开单人名片
-QTALK_OPEN_GROUP_VCARD = 1  # 打开群组聊天
-QTALK_OPEN_FRIENDS_VC = 2  # 打开好友
-QTALK_OPEN_GROUPS_VC = 3  # 打开群组
-QTALK_OPEN_UNREAD_MESSAGE = 4  # 打开未读消息
-QTALK_OPEN_PUBLIC_ACCOUNT = 5  # 打开公众号
-QTALK_WEBVIEW = 6  # 打开webview，渲染url
-QTALK_OPEN_USER_CHAT = 7  # 打开单人聊天
-QTALK_OPEN_PUBLIC_VCARD = 8  # 打开公众号名片
-
-ip = str()
-s_args = dict()
+sharemsg_blueprint = Blueprint('sharemsg', __name__, template_folder='../../templates', static_folder='../../static')
 
 
-@search_blueprint.route('/search', methods=['GET', 'POST'])
-def main():
-    global ip, s_args
-    args = RequestUtil.get_request_args(request)
-    if not s_args:
-        s_args = args
-    try:
-        username = args["key"].strip()
-        user_id = args["qtalkId"].strip()
-    except KeyError:
-        return jsonify(ret=0, message="wrong parameters")
-    if len(username) < 2:
-        return jsonify(ret=0, message="length of key should greater than two")
-    ckey = args.get("cKey", "")
-    group_id = args.get("groupid", "")
-    offset = int(args.get("start", 0))
-    limit = int(args.get("length", 5))
-    if is_check_ckey:
-        res, user = check_ckey(ckey, user_id)
-        if not res:
-            logger.error('{} login FAILED. ckey : {}'.format(user_id, ckey))
-            return jsonify(ret=0, message="ckey failed")
-
-    request_ip = request.remote_addr
-    if ip != request_ip:
-        ip = request_ip
-        logger.info(ip + ' :  {}'.format(json.dumps(s_args)))
-    s_args = args
-    result = list()
-    data_array = list()
-    userlib = UserLib()
-    if group_id:
-        if group_id == userGroup:
-            user_array = userlib.search_user(username, user_id, limit + 1, offset)
-            has_more, user_array = get_hasmore(user_array, limit)
-            if user_array:
-                result = make_result(label='联系人列表', groupid=userGroup, group_priority=0, todo_type=QTALK_OPEN_USER_VCARD,
-                                 portrait=single_portrait, hasmore=has_more, info=user_array)
-        elif group_id == groupGroup:
-            group_array = userlib.search_group(user_id, username, limit + 1, offset)
-            has_more, group_array = get_hasmore(group_array, limit)
-            if group_array:
-                result = make_result(label='群组列表', groupid=groupGroup, group_priority=0, todo_type=QTALK_OPEN_GROUP_VCARD,
-                                 portrait=muc_portrait, hasmore=has_more, info=group_array)
-        elif group_id == commonGroup:
-            commonmuc_array = userlib.search_group_by_single(user_id, username, limit + 1, offset)
-            has_more, commonmuc_array = get_hasmore(commonmuc_array, limit)
-            if commonmuc_array:
-                result = make_result(label='共同群组', groupid=commonGroup, group_priority=0, todo_type=QTALK_OPEN_GROUP_VCARD,
-                                 portrait=muc_portrait, hasmore=has_more, info=commonmuc_array)
-        else:
-            return jsonify(errcode=500, msg='无法预期的groupId')
-        userlib.conn.close()
-        return jsonify(errcode=0, msg='', data=result)
-    else:
-        user_array = userlib.search_user(username, user_id, limit + 1, offset)
-        group_array = userlib.search_group(user_id, username, limit + 1, offset)
-        commonmuc_array = userlib.search_group_by_single(user_id, username, limit + 1, offset)
-        if user_array:
-            has_more, user_array = get_hasmore(array=user_array, limit=limit)
-            result = make_result(label='联系人列表', groupid=userGroup, group_priority=0, todo_type=QTALK_OPEN_USER_VCARD,
-                                 portrait=single_portrait, hasmore=has_more, info=user_array)
-            data_array.append(result)
-        if group_array:
-            has_more, group_array = get_hasmore(array=group_array, limit=limit)
-            result = make_result(label='群组列表', groupid=groupGroup, group_priority=0, todo_type=QTALK_OPEN_GROUP_VCARD,
-                                 portrait=muc_portrait, hasmore=has_more, info=group_array)
-            data_array.append(result)
-        if commonmuc_array:
-            has_more, commonmuc_array = get_hasmore(commonmuc_array, limit)
-            result = make_result(label='共同群组', groupid=commonGroup, group_priority=0, todo_type=QTALK_OPEN_GROUP_VCARD,
-                                 portrait=muc_portrait, hasmore=has_more, info=commonmuc_array)
-            data_array.append(result)
-        userlib.conn.close()
-        return jsonify(errcode=0, msg='', data=data_array)
+# sharemsg_blueprint = Blueprint('sharemsg', __name__)
 
 
-def make_result(label, groupid, group_priority, todo_type, portrait, hasmore, info):
-    if not info:
-        info = ''
-    _result = {
-        'groupLabel': label,
-        'groupId': groupid,
-        'groupPriority': group_priority,
-        'todoType': todo_type,
-        'defaultportrait': portrait,
-        'hasMore': hasmore,
-        'info': info
-    }
-    return _result
+def authorization(func):
+    def wrapper(*args, **kw):
+        ckey = ''
+        user_id = 'DEFAULT'
+        request_util = RequestUtil()
+        res = False
+        if is_check_ckey:
+            ckey = request_util.get_ckey(request)
+            sharemsg_logger.debug("user {} , get ckey : {}".format(user_id, ckey))
+            if ckey:
+                if auth_ckey_url:
+                    try:
+                        r_data = {
+                            'ckey': ckey,
+                            'system': 'sharemsg'
+                        }
+                        ret = requests.post(url=auth_ckey_url, json=r_data)
+                        if ret.json().get('ret'):
+                            if ret.json().get('data').get('d') != r_domain:
+                                return jsonify(ret=0, message="Error domain")
+                            res = True
+                            user_id = ret.json().get('data').get('u')
+                    except (requests.RequestException or KeyError) as e:
+                        sharemsg_logger.error("ckey api failed : {}".format(e))
+                        # TODO notify developer to check
+                        # res = check_ckey(ckey, user_id)
+                        res, user_id = check_ckey(ckey)
+
+                    except Exception as e:
+                        logger.error("ckey api failed : {}".format(e))
+                else:
+                    sharemsg_logger.debug("user {} , get ckey : {}".format(user_id, ckey))
+                    res, user_id = check_ckey(ckey)
+            if res:
+                return func(user_id=user_id, *args, **kw)
+            else:
+                sharemsg_logger.info("user:{user} login failed, ckey : {ckey}, \
+                                                ".format(user=user_id, ckey=ckey))
+                return jsonify(ret=0, message="ckey check failed")
+        return func(user_id=user_id, *args, **kw)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 
-def get_hasmore(array, limit=5):
-    has_more = False
-    if array:
-        item_count = len(array)
-        if item_count > limit:
-            has_more = True
-            array.pop()
-        return has_more, array
-    else:
-        return has_more, array
+@sharemsg_blueprint.route('sharemsg')
+@authorization
+def main(user_id):
+    sharemsg_logger.info("Get request from " + user_id)
+    handler = GenerateShareMsg()
+    content = handler.show()
+    return render_template('sharemsg_template.html', content=content)
 
-#
-# if __name__ == '__main__':
-#     app.run(host=service_host, port=service_port, debug=True)
+
+class GenerateShareMsg:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def pad_data(_data):
+        _data = _data + '=' * (len(_data) % 4)
+        return _data
+
+    @staticmethod
+    def form_data(_data):
+        """
+        - -> +
+        _ -> /
+        . -> +
+        :param _data:
+        :return:
+        """
+        if '-' in _data:
+            _data = _data.replace('-', '+')
+        if '_' in _data:
+            _data = _data.replace('_', '/')
+        if '.' in _data:
+            _data = _data.replace('.', '+')
+        return _data
+
+    def show(self):
+        jdata = request.args.get('jdata', '')
+        if not jdata:  # TODO 更好的处理方式
+            return NOT_FOUND_ERROR
+        if ('-' in jdata) or ('_' in jdata) or ('.' in jdata):
+            jdata = self.form_data(jdata)
+        try:
+            data_url = base64.b64decode(jdata)
+        except:
+            jdata = self.pad_data(jdata)
+            try:
+                data_url = base64.b64decode(jdata)
+            except Exception as e:
+                sharemsg_logger.exception("Cant decode base64 :" + jdata)
+                return
+        result = requests.get(data_url)
+        if not result.ok:
+            return OUTDATED_ERROR
+        try:
+            content = json.loads(result.text)
+        except Exception as e:
+            sharemsg_logger.exception("Json load failed \n {}".format(e))
+            return
+        main_region = ""
+        content.sort(key=lambda x: x['s'])
+        util = Utility()
+        content = util.handle_sharemsg_timeinterval(content)
+        for item in content:
+            msg = util.handle_sharemsg(item)
+            main_region += "<div>" + item.get('time_html', '')
+            speaker_div = util.handle_sharemsg_speaker(item).format(content=msg, name=item.get('n', ''))
+            main_region += speaker_div + "</div>"
+        return main_region
+
